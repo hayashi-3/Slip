a77ab608c386f3194ec07b1ac844178ed6bf566b
hayashi-3 <hayashi.gml@gmail.com>
2022-01-03 05:26:52 +0000

    [fix]MonthSummaryInsert

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -21,7 +21,7 @@ return [
     'alpha_dash' => 'The :attribute may only contain letters, numbers, dashes and underscores.',
     'alpha_num' => 'The :attribute may only contain letters and numbers.',
     'array' => 'The :attribute must be an array.',
-    'before' => ':attribute は今日より後の日付で入力してください。',
+    'before' => ':attribute は今日より前の日付で入力してください。',
     'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
     'between' => [
         'numeric' => 'The :attribute must be between :min and :max.',
